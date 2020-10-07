7f29402c75d4bb33165ae37ed992197f0816ce33
damianIracki <damian.iracki13@gmail.com>
2020-10-07 21:03:21 +0200

    added new tasks

--- task12/task12a - toDoList/app.js
+++ task12/task12a - toDoList/app.js
@@ -1,9 +1,60 @@
-const removeTask = (e) => {
-    const index = e.target.dataset.key;
-    document.querySelector(`li[data-key="${index}"]`).remove();
+let toDoList = [];
 
+const form = document.querySelector("form");
+const ul = document.querySelector("ul");
+const span = document.querySelector("h1 span");
+const listItems = document.getElementsByClassName('task');
+const input = document.querySelector("input");
+
+const renderList = (array) => {
+    ul.textContent = "";
+
+    array.forEach((toDoElement, key) => {
+        toDoElement.dataset.key = key;
+        ul.appendChild(toDoElement)
+    })
+}
+
+const searchTask = (e) => {
+    let searchingTask = e.target.value.toLowerCase();
+    console.log(searchingTask);
+
+    let tasks = [];
+    tasks = toDoList.filter(task => task.innerText.toLowerCase().includes(searchingTask));
+    console.log(tasks);
+
+    if (e.target.value === "") renderList(toDoList);
+
+    renderList(tasks);
+}
+
+const removeTask = (e) => {
+    const index = e.target.parentNode.dataset.key;
+    toDoList.splice(index, 1);
+    span.textContent = toDoList.length;
+
+    renderList(toDoList);
+}
+
+const addTask = (e) => {
+    e.preventDefault();
+    const taskName = input.value;
+    if (taskName === "") return;
+    const newTask = document.createElement('li');
+    newTask.className = "task";
+    newTask.innerHTML = taskName + ' <button class="delete">Delete</button>';
+    toDoList.push(newTask);
+
+    renderList(toDoList);
+
+    ul.appendChild(newTask);
+    input.value = "";
+    span.textContent = listItems.length;
+
+    document.querySelector("li:last-child button").addEventListener("click", removeTask);
 
 }
 
-const list = document.querySelectorAll(`button[data-key]`);
-list.forEach(item => item.addEventListener("click", removeTask));
+
+input.addEventListener("input", searchTask)
+form.addEventListener("submit", addTask);
